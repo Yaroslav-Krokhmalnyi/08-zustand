4dbc08a2db9a57c2d374da341a0f253f64a47f25
Yaroslav Krokhmalnyi <krokhmalniy.code@gmail.com>
2026-01-29 22:07:44 +0200

    Create metadata for global layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-// app/layout.tsx
+// Meta
+import { Metadata } from 'next';
 
 //Styles
 import "./globals.css";
@@ -8,10 +9,24 @@ import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "NoteHub",
   description: "A simple notes application built with Next.js",
-};
+  openGraph: {
+      title: "NoteHub",
+      description: "A simple notes application built with Next.js",
+      url: 'https://yaroslav-krokhmalnyi.github.io/08-zustand/',
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: 'NoteHub — a simple web-based note-taking application built with Next.js',
+        },
+      ],
+      type: 'website',
+    },
+  };
 
 export default function RootLayout({
   children,
